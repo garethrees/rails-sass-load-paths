3ca4e8fcd89b93fde60b069eac18367d3944b137
Gareth Rees <gareth@garethrees.co.uk>
2016-10-14 14:14:25 +0100

    Update to r4 and run rake rails:update

--- r4/config/initializers/secret_token.rb
+++ r4/config/initializers/secret_token.rb
@@ -1,8 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying the integrity of signed cookies.
+# Your secret key is used for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-R3::Application.config.secret_token = '71acb3d63d235ed7bcac82f89d8b37e405937683510699b46c6d0805caf027f8cc8aa86b19580e24a37e3cd57777e1c641ad12763b6a9a87eae316b7f9a415af'
-R3::Application.config.secret_key_base = '1234'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+R3::Application.config.secret_key_base = '0b1f0f2286aabf36e9d3361ebd4d7ff4e94618d9d80f339728237b6e60889c0a36908d72ffa2eb9c9472bd80e6d293082d09d5d81c997e19367b0fcb62b92a5b'
