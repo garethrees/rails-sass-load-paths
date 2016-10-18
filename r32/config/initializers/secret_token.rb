5c2c854b1dfb94aea1d7910546203fbd01fbba50
Gareth Rees <gareth@garethrees.co.uk>
2016-10-18 14:25:09 +0100

    Add Rails 3.2 app

---
+++ r32/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+R32::Application.config.secret_token = '589ed37bb08ca76336631aadfa123cabad589f9eccb5857c0fdaba076a3c82227b983996254710a7f95db0144a0e5141cb1bff4de0430d3e982527060bb364fc'
